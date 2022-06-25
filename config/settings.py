9d958dc5f5a3fe18cbd39b4c2cadbdac557c9840
Gabriel-Nunes <gabriel_lima_nunes@hotmail.com>
2022-06-25 20:23:54 -0300

    heroku deploy

--- config/settings.py
+++ config/settings.py
@@ -14,11 +14,10 @@ with open('secret_key.txt') as file:
     SECRET_KEY = file.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '0.0.0.0',
-    '127.0.0.1',
+
     ]
 
 
