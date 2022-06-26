622bbf33909abbeef078ba0158233f2653e5869f
Gabriel-Nunes <gabriel_lima_nunes@hotmail.com>
2022-06-26 19:04:51 -0300

    Automatic configuration with django-on-heroku

--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku config
+import django_on_heroku
+django_on_heroku.settings(locals())
