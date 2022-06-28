5a4f10a12291ea3577f9706d2877692706f7d0fc
Gabriel-Nunes <gabriel_lima_nunes@hotmail.com>
2022-06-27 22:56:28 -0300

    Disable collectstatic

--- config/settings.py
+++ config/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+DISABLE_COLLECTSTATIC=0
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = BASE_DIR / "staticfiles"
